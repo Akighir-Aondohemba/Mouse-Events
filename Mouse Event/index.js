78e689cf99384513c47fb965a89c84e46a6eb401
Akighir Aondohemba Joseph <118387506+Akighir-Aondohemba@users.noreply.github.com>
2023-05-21 00:22:13 +0100

    Add files via upload

---
+++ Mouse Event/index.js
@@ -0,0 +1,14 @@
+ const containerEl = document.querySelector(".container");
+
+ window.addEventListener("mousemove", (event)=>{
+    containerEl.innerHTML = `
+    <div class="mouse-event">
+    ${event.clientX}
+    <h4>Mouse X Position(px)</h4>
+</div>
+<div class="mouse-event">
+    ${event.clientY}
+    <h4>Mouse Y Position(py)</h4>
+</div>
+    `
+ });
